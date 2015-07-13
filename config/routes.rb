a16f44de04895859784622b74638935455902e12
JuanHenao <juan-o-henao@hotmail.es>
2015-07-13 20:13:21 +0000

    add car

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'cart/index'
-
+  
+  get '/cart' => 'cart#index' 
+  
+  get '/cart/clear' => 'cart#clearCart'
+  
+  get '/cart/:id' => 'cart#add'
+   
   get 'controller/cart'
 
   get 'controller/index'
